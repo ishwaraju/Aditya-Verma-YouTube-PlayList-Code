e31ff5fff18cd83ebf89c6c458fdfe720117d866
Ishwar Kumar <ishwar@ishwars-air.home>
2022-07-30 17:05:28 +0100

    Initial Commit for project

---
+++ src/main/java/com/ishwaraju/AdityaVermaYouTubePlayListCodeApplication.java
@@ -0,0 +1,13 @@
+package com.ishwaraju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdityaVermaYouTubePlayListCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdityaVermaYouTubePlayListCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishwaraju/AdityaVermaYouTubePlayListCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishwaraju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdityaVermaYouTubePlayListCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
